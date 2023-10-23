c340c5d5c238c07474aaf0a17cc1a6a7883e78ac
CristhianSepulveda <108998095+CristhianSepulveda@users.noreply.github.com>
2023-10-23 13:00:25 -0500

    Update newBooking.php

--- bookings/newBooking.php
+++ bookings/newBooking.php
@@ -13,16 +13,21 @@
     <form action="procesar_reserva.php" method="post">
         <label for="sucursal">Selecciona una sucursal:</label>
         <select name="sucursal" id="sucursal">
-            <option value="sucursal1">Sucursal 1</option>
-            <option value="sucursal2">Sucursal 2</option>
-            <option value="sucursal3">Sucursal 3</option>
+            <option value="sucursal1">Bogotá (Principal)</option>
+            <option value="sucursal2">Bucaramanga</option>
+            <option value="sucursal3">Medellin</option>
+            <option value="sucursal4">Cali</option>
         </select>
 
         <label for="servicio">Selecciona el servicio a reservar:</label>
         <select name="servicio" id="servicio">
-            <option value="servicio1">Servicio 1</option>
-            <option value="servicio2">Servicio 2</option>
-            <option value="servicio3">Servicio 3</option>
+            <option value="servicio1">Habitaciones</option>
+            <option value="servicio2">Auditorio</option>
+            <option value="servicio3">Zona Humeda</option>
+            <option value="servicio4">Gimnasio</option>
+            <option value="servicio5">Restaurante</option>
+            <option value="servicio6">Salón de eventos</option>
+            <option value="servicio7">Gimnasio</option>
         </select>
 
         <label for="fecha">Fecha de reserva:</label>
